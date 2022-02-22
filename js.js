2dd5a417e5885e7e7f933692d23b3fe65e2aa00f
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-02-22 21:46:00 +0300

    Глава 1

--- js.js
+++ js.js
@@ -140,3 +140,26 @@ while (i < 2) {
 	i = i + 1;
 } document.write("Happy Birthday dear " + name + ",<br>");
   document.write("Happy Birthday to you.<br>");
+
+Близкое знакомство с console.log
+  
+  
+  var messaage = "Howdy " + "partner";
+  console.log(messaage);
+  
+  */
+  
+  
+	var word = "bottles";
+	var count = 5;
+	while (count > 0) {
+		console.log(count + " " + word + " of beer on the wall");
+		console.log(count + " " + word + " of beer,");
+		console.log("Take one down, pass it around,");
+		count = count - 1;
+	if (count > 0) {
+		console.log(count + " " + word + " of beer on the wall.");
+ } else {
+		console.log("No more " + word + " of beer on the wall.");
+ }
+}
