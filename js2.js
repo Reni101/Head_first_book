f4caea7272dad11f87eb5e8e5a1bf5fad0176fd3
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-02-23 16:05:07 +0300

    Update js2.js

--- js2.js
+++ js2.js
@@ -6,11 +6,52 @@ var hits = 0;	// количество попадайний
 var guesses = 0; // количество попыток
 var isSunk = false; //переменная для хранения информации о том, потоплен корабль или нет
 
-while(isSunk == false){
-	guess=prompt('Введите кординаты(0-6):', ' '));
-	if (guess <0 || guess > 6){
+
+
+while (isSunk == false) {
+	guess = prompt('Введите кординаты(0-6):');
+	if (guess < 0 || guess > 6){
 		alert('введи блин нужные цифры, чОрт');
 	} else {
 		guesses = guesses + 1;
-	}
+		if (guess == location1 || guess == location2 || guess == location3 ){
+		hits = hits + 1;
+		alert("HIT!");
+		if (hits == 3){
+		isSunk = true;
+		alert('You sank my battleship!');
 }
+	} else { alert("MISS");}
+			}
+				}
+					
+var stats = "You took " + guesses + " guesses to sink the battleship, " +
+ "which means your shooting accuracy was " + (3/guesses);
+alert(stats);
+
+
+
+
+
+/*
+if (guess == location1 ){
+	hits = hits + 1;
+} else if (guess == location2){
+	hits = hits + 1;
+} else if (guess == location3){
+	hits = hits + 1;
+}
+
+
+
+if (guess == location1 || guess == location2 || guess == location3 ){
+	hits = hits + 1;
+}
+
+if (hits == 3){
+	isSunk = true;
+	alert('You sank my battleship!');
+}
+
+
+*/
