318b92d1e9b0a3b3dc29c4c68c9d6abd56b9c3f3
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-02-23 17:39:14 +0300

    Play battleship! 2 глава окончена

--- js2.js
+++ js2.js
@@ -1,6 +1,12 @@
+/*
 var location1 = 3; // переменные для хранения каждого корабля
 var location2 = 4;
-var location3 = 5;
+*/
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1+1
+var location3 = location2+1
+
 var guess; 		// переменная для номера текущей попытки
 var hits = 0;	// количество попадайний
 var guesses = 0; // количество попыток
@@ -32,8 +38,8 @@ alert(stats);
 
 
 
-
 /*
+
 if (guess == location1 ){
 	hits = hits + 1;
 } else if (guess == location2){
@@ -53,5 +59,52 @@ if (hits == 3){
 	alert('You sank my battleship!');
 }
 
+
+
+
+
+var temp = 81;
+var willRain = true;
+var humid = (temp > 80 && willRain == true);
+// humid = true
+
+var guess = 6;
+var isValid = (guess >= 0  && <=0 )
+// isValid = true
+
+var kB = 1287;
+var tooBig = (kB > 1000); /true 
+var urgent = true;
+var sendFile = (urgent == true || tooBig == false);
+// sendFile =  true == urgent 
+
+var keyPressed = "N";
+var points = 142;
+var level;
+if (keyPressed == "Y" ||(points > 100 && points < 200)){
+	level = 2;
+} else {
+	level = 1;
+}
+// level = 2
+
+
+
+
+
+if (price < 200 || price > 600){
+	alert('price is too low or too high! Don\'t by the gadget. ')
+} else {
+	alert("price is right! Buy the gadget.");
+}
+
+
+var price = 3250
+
+if (price >= 200 || price <= 600) {  // надо заменить || на && что бы работало коректно
+	alert("price is right! Buy the gadget.");
+} else{
+	alert('price is too low or too high! Don\'t by the gadget. ')
+}
 
 */
