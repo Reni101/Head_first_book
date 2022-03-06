bdfd482b9ed4ca6dcd4fac085f9e49110ab0d630
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-03-06 18:06:52 +0300

    Update js3.js

--- js3.js
+++ js3.js
@@ -53,7 +53,7 @@ if (dogWeight > 20) {
 bark("scottie", -1);
 bark("dino", 0, 0); // почему работает?
 
- */
+ 
  
  function whatShallIWear(temp){
 	 if (temp < 60) {
@@ -68,3 +68,94 @@ bark("dino", 0, 0); // почему работает?
  whatShallIWear(60);
  whatShallIWear(50);
  whatShallIWear(80);
+ 
+ 
+
+
+
+function saveMyProfile(name, birthday, GPA, newuser){
+	if(birthday >= 2004) {
+		// для родившихся в 2004 году и позже...
+	} // остальная часть функции
+}
+
+saveMyProfile("Krissy", 1991, 3.81, fa;se);
+// каждый аргумент присваивается соответсвтующему параметру функции.
+
+Переменные тоже могут передаваться как аргументы, причем этот способ применяется
+даже чаще непосредственной передачи значений.
+Вызов той же функции с использованием переменных:
+ 
+var student = "krissy";
+var year = 1991;
+var GPA = 381/100;
+var status = "existinguser"
+var isNewUser = (status == "newuser");
+saveMyProfile(student, year, GPA,isNewUser );
+
+
+В качестве аргументов могут использоваться
+даже выражения:
+
+saveMyProfile(student, year, 381/100, status == "newuser");
+
+В каждом случае программа сначала вычисляет выражение,
+получает конкретное значение и передает его функции.
+
+
+
+function dolt(param){
+	param = 2;
+}
+var tes = 1;
+dolt(tes);
+console.log(tes);
+// почему 1 ????
+
+function dogYears(dogName, age){
+	var years = age * 7;
+	console.log(dogName = "is " + years + "years old");
+}
+var myDog = "Fido";
+dogYears(myDog, 4)
+
+*/
+
+function dogYears(dogName,age) {
+	var years = age * 7;
+	console.log(dogName = " is " + years = " years old");
+}
+var myDog ="Fido";
+dogYears(myDog, 4);
+
+function makeTea(cups, tea) {
+	console.log("Brewing " + cups + " cups of" + tea);
+}
+
+var guests = 3;
+makeTea(guests, "Earl Grey");
+
+function secret() {
+	console.log("The secret of life is 42")
+}
+secret();
+
+function speak(kind) {
+	var defaultSound = "";
+	if (kind == "dog"){
+		alert("Woof");
+	} else if (kind == "cat"){
+		alert("Meow");
+	} else {
+		alert(defaultSound);
+	}
+}
+
+var pet = prompt ("Enter a type of pet:");
+speak(pet);
+
+// функции : dogYears  makeTea   secret  speak
+// переменные  years myDog guests defaultSound  pet
+//Встроенные функции :  alert prompt
+// Аргументы : myDog 4  guests "Earl Grey" pet
+// Параметры :  dogName  age   cups  tea kind
