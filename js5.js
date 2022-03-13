938267f140ef07c8d777d8082dffda9a1254fcdf
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-03-13 21:15:55 +0300

    Глава 5 Объекты

--- js5.js
+++ js5.js
@@ -87,7 +87,6 @@ console.log(speak);
 fido.dogYears = 35;
 delete fido.dogYears;
 
-*/
 
 
 var taxi = {
@@ -148,3 +147,36 @@ if(worthALook){
 	console.log("you should really pass ont the" +taxi.make + " " + taxi.model)
 }
 
+*/
+
+
+function getSecret(file, secretPassword){
+	file.opened = file.opened + 1;
+	if(secretPassword == file.password){
+		return file.contents.;
+	} else {
+		return "invalid password! No secret for you.";
+	}
+}
+
+function setSecret(file, secretPassword, secret){
+	if (secretPassword == file.password){
+		file.opened = 0;
+		file.contents = secret;
+	}
+}
+
+var superSecretFile = {
+ level: "classified",
+ opened: 0,
+ password: 2,
+ contents: "Dr. Evel's next meeting is in Detroit."
+
+
+var secret = getSecret(superSecretFile,superSecretFile.password)
+console.log(secret);
+
+
+setSecret(superSecretFile, 2, "Dr. Evel's next meeting is in Philadelphia.");
+secret = getSecret(superSecretFile, 2);
+console.log(secret);
