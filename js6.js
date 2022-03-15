9c8bbb501b9c942f429693cb954e7b0c88afad24
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-03-15 16:16:10 +0300

    Глава 6 DOM

---
+++ js6.js
@@ -0,0 +1,35 @@
+/*
+var acces =
+	document.getElementById('code9');
+var code = acces.innerHTML;
+code = code + " midnight";
+alert(code);
+
+ 	
+function init() {
+ var planet = document.getElementById("greenplanet");
+ planet.innerHTML = "Red Alert: hit by phaser fire!";
+}
+window.onload = init;
+
+	
+
+
+var planet = document.getElementById("greenplanet"); 
+planet.innerHTML = "Red Alert: hit by phaser fire!!!!";
+console.log(planet.innerHTML);
+
+*/
+
+
+function addSongs(){
+	var song1 = document.getElementById("song1");
+	var song2 = document.getElementById("song2");
+	var song3 = document.getElementById("song3");
+	
+	song1.innerHTML = "Blue Suede Strings, by Elvis Pagely";
+	song2.innerHTML = "Great Objects on Fire, by Jerry JSON Lewis";
+	song3.innerHTML = "I Code the Line, by Johnny JavaScript";
+}
+	window.onload = addSongs;
+	
