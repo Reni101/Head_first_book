9c62b29c84b29da0085f4d1a584a5db78e862ad9
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-03-16 12:00:44 +0300

    Глава 6 DOM

--- js6.js
+++ js6.js
@@ -15,11 +15,9 @@ window.onload = init;
 	
 
 
-var planet = document.getElementById("greenplanet"); 
-planet.innerHTML = "Red Alert: hit by phaser fire!!!!";
-console.log(planet.innerHTML);
 
-*/
+
+
 
 
 function addSongs(){
@@ -32,4 +30,36 @@ function addSongs(){
 	song3.innerHTML = "I Code the Line, by Johnny JavaScript";
 }
 	window.onload = addSongs;
+
 	
+var planet = document.getElementById("greenplanet"); 
+planet.innerHTML = "Red Alert: hit by phaser fire!!!!";
+console.log(planet.innerHTML);
+
+
+planet.setAttribute("class", "redtext")
+
+//Используем метод setAttribute для добавления нового или изменения существующего атрибута.
+
+
+var scoop = document.getElementById("raspberry");
+var alignText = scoop.getAttribute('align');
+console.log("I can't see the image in the console,");
+console.log(" but I'm told it looks like: " + alignText);
+
+var scoop = document.getElementById("raspberry")
+var altText = scoop.getAttribute("alt");
+if(altText == null){
+	console.log("Oh, I guess there isn't an alt attribute.");
+}else {
+	console.log("I can't see the image in the console,");
+	console.log(" but I'm told it looks like: " + altText);
+}
+
+*/
+function init() {
+	var planet = document.getElementById("greenplanet");
+		planet.innerHTML = "Red Alert: hit by phaser fire!";
+		planet.setAttribute("class", "redtext");
+ }
+	window.onload = init;
