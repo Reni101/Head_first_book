c0163b8e12a01dddee39badad8833a13e3a63688
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-03-21 14:28:36 +0300

    Update js8.js

--- Глава Battleship/js8.js
+++ Глава Battleship/js8.js
@@ -66,7 +66,7 @@ var model = {
 	shipsSunk: 0,
 	ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
 			{ locations: ["24", "34", "44"], hits: ["", "", ""] },
-			{ locations: ["10", "11", "12"], hits: ["", "", ""] }]
+			{ locations: ["10", "11", "12"], hits: ["", "", ""] }],
 
 // выстрел
 	fire: function(guess) {
@@ -75,7 +75,10 @@ var model = {
 		var index = ship.locations.indexOf(guess);
 		if(index >= 0) {
 			ship.hits[index] = "hit";
+			view.displayHit(guess);
+			view.displayMessage("HIT!");
 			if(this.isSunk(ship)) {
+				view.displayMessage("You sank my battleship!");
 				this.shipsSunk++;
 			}
 			return true;
@@ -83,7 +86,7 @@ var model = {
 		}
 	}
 		return false;
-	};
+	},
 	// проверка на потопление
 	isSunk: function(ship) {
 	for(var i =0; i < this.shipLength; i++) {
@@ -92,7 +95,7 @@ var model = {
 		}
 	}
 	return true;
-	};
+	}
 };
 /* 
 Метод получает координаты выстрела. Затем мы перебираем массив
@@ -101,3 +104,58 @@ ships, последовательно проверяя каждый корабл
 координаты выстрела с координатами одной из занимаемых им клеток.
 */
 
+var controller = {
+	guesses: 0,
+	processGuess: function(guess) {
+		var location = parseGuess(guess);
+		if (location) {
+			this.guesses++;
+			var hit = model.fire(location);
+			if (hit && model.shipsSunk === model.numShips){
+				view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+
+			}
+		}
+	}
+};
+	
+	function parseGuess(guess){
+		var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+		if (guess === null || guess.length !== 2) {
+			alert("Oops, please enter a letter and a number on the board.");
+		}else {
+			firstChar = guess.charAt(0);
+			var row = alphabet.indexOf(firstChar);
+			var column = guess.charAt(1);
+			
+			if(isNaN(row) || isNaN(column)) {
+				alert("Oops, that isn't on the board.");
+			} else if(row < 0 || row > model.boardSize ||
+					  column >= model.boardSize) {
+						  alert("Oops, that's off the board!");
+			} else {
+				return row + column;
+			}
+
+		} return null;
+	}
+		
+	
+	/*
+console.log(parseGuess("A0"));
+console.log(parseGuess("B6"));
+console.log(parseGuess("G3"));
+console.log(parseGuess("H0"));
+console.log(parseGuess("A7"));
+ */
+ 
+controller.processGuess("A0");
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
