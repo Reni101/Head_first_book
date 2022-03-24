8618c4c246aa1f37bbe061138941b79c2d58ea1c
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-03-24 16:41:00 +0300

    Update js9.js

--- Глава 9/js9.js
+++ Глава 9/js9.js
@@ -11,7 +11,38 @@ function init(){
 
 function showAnswer(eventObj) {
 	var image = eventObj.target; //Свойство target сообщает, какой элемент сгенерировал данное событие. 
-	var name =image.id;
+	var name = image.id;
 	name = "img9/" + name +  ".jpg";
 	image.src = name;
+	setTimeout(reblur, 2000, image);
 }
+
+function reblur(image){
+	var name = image.id;
+	name = "img9/" + name + "blur.jpg";
+	image.src = name;
+}
+/*
+window.onload = init;
+
+function init(){
+	var map = document.getElementById('map')
+	map.onmousemove = showCoord; // map.onclick = showCoord;
+}
+
+function showCoord(eventObj){
+	var map = document.getElementById('coords');
+	var x =eventObj.clientX;
+	var y =eventObj.clientY;
+	map.innerHTML = "Map coordinates: "+ x + ", " + y;
+
+}
+
+
+
+function timerhandLer(){
+	alert('Hey what are you doing just sitting there staring at a blank screen?')
+}
+
+setTimeout(timerhandLer, 3000);
+*/
