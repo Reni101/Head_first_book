bd32684a92799cfb8296c73deb6c1205d51ae4fc
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-03-26 14:04:33 +0300

    Update js10.js

--- Глава 10 Первокласные функции/js10.js
+++ Глава 10 Первокласные функции/js10.js
@@ -14,7 +14,7 @@ if (migrating) {
  quack(4);
  fly(4);
 }
-*/
+
 var winner = function() { alert("WINNER!") };
 var loser = function() { alert("LOSER!") };
 
@@ -34,3 +34,109 @@ a = c;
 a = b;
 b = c;
 a();
+
+
+
+
+
+function checkNoFlyList(passenger){
+	if(passenger.name === "Dr. Evel" ) {
+		return true;
+	} else {
+		return false; //пассажир не входит в «черный список»
+	}
+};
+
+function chekNotPaid(passenger) {
+	if(!passenger.paid){ //Если билет не оплачен, возвращаем true.
+		return true;
+	} else{
+		return false;
+	}
+}
+
+function processPassengers(passenger, testFunction) {
+	for (var i = 0; i < passenger.length; i++){
+		if(testFunction(passenger[i])){
+			return false;
+		}
+	} return true;
+}
+var allCanFly = processPassengers(passengers,checkNoFlyList);
+if (!allCanFly){
+	console.log("The plane can't take off: we have a passenger on the no-fly-list.")
+};
+
+var allPaid = processPassengers(passengers, chekNotPaid);
+if(!allPaid){
+	console.log("The plane can't take off: not everyone has paid.");
+};
+
+function printPassenger(passenger){
+	var  message = passenger.name;
+	if (passenger.paid === true) {
+		message = message + " has paid";
+	} else {
+		message = message +" has not paid";
+	}
+	console.log(message);
+};
+
+processPassengers(passengers, printPassenger);
+
+
+
+
+var passengers = [  { name: "Jane Doloop", paid: true, ticket: "coach" },
+ 					{ name: "Dr. Evel", paid: true, ticket: "firstclass" },
+ 					{ name: "Sue Property", paid: false, ticket: "firstclass" },
+ 					{ name: "John Funcall", paid: true, ticket: "coach" } ];
+
+
+
+function createDrinkOrder(passenger) {
+	var orderFunction;
+	if(passenger.ticket === "firstclass") {
+		orderFunction = function() {
+			console.log("Would you like a cocktail or wine?");
+};
+	} else { 
+		orderFunction = function() {
+			console.log("Your choice is cola or water.");
+			};
+	}
+	return orderFunction;
+}
+
+
+function serveCustomer(passenger) {
+	var getDrinkOrderFunction = createDrinkOrder(passenger);
+	getDrinkOrderFunction();
+	// Предложить обед
+	getDrinkOrderFunction();
+	getDrinkOrderFunction();
+	// Включить кино
+	getDrinkOrderFunction();
+	// Забрать мусор
+
+}
+
+function servePassengers(passenger) {
+	for (var i = 0; i < passenger.length; i++) {
+		serveCustomer(passenger[i]);
+	}
+}
+
+servePassengers(passengers);
+
+*/
+
+var products = [ { name: "Grapefruit", calories: 170, color: "red", sold: 8200 },
+ { name: "Orange", calories: 160, color: "orange", sold: 12101 },
+ { name: "Cola", calories: 210, color: "caramel", sold: 25412 },
+ { name: "Diet Cola", calories: 0, color: "caramel", sold: 43922 },
+ { name: "Lemon", calories: 200, color: "clear", sold: 14983 },
+ { name: "Raspberry", calories: 180, color: "pink", sold: 9427 },
+ { name: "Root Beer", calories: 200, color: "caramel", sold: 9909 },
+ { name: "Water", calories: 0, color: "clear", sold: 62123 }
+ ];
