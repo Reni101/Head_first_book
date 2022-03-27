7920f5c0301ee3ce0c6d7b95db1ec5ce9b4657b0
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-03-27 20:59:27 +0300

    Update js11.js

--- Глава 11 Анонимные функции/js11.js
+++ Глава 11 Анонимные функции/js11.js
@@ -1,1 +1,57 @@
+/*
 window.onload = function() {alert("Yeah, that page loaded!");}
+
+function vaccine(dosage) {
+	if (dosage > 0) {
+	inject(dosage);
+	}
+   }
+   administer(patient, vaccine, time);
+
+   administer(patient,
+			 function(dosage){
+						if (dosage > 0) {inject(dosage);
+}
+			},
+				time
+)
+
+
+
+function makeCounter() {
+	var count = 0;
+ function counter() {
+	count = count + 1;
+	return count;
+}
+	return counter;
+};
+   var doCount = makeCounter();
+   console.log(doCount());
+   console.log(doCount());
+   console.log(doCount());
+
+*/
+
+function makePassword(password) {
+	return function guess (passwordGuess) {
+		return (passwordGuess === password);
+	};
+}
+
+function multN(n) {
+	return function mult(x) {
+		return  x * n;
+	};
+}
+
+
+function makeCounter() {
+	var count = 0;
+	
+	return function inc() {
+		count = count++;
+		return count
+	}
+
+   }
