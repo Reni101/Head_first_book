7f98eacd65025e4b0c856e5cedf7a6c214a7391a
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-03-29 09:30:24 +0300

    Add files via upload

---
+++ Глава 12 Конструкторы/js12.js
@@ -0,0 +1,27 @@
+function Dog (name, breed, weight) {
+	this.name = name;
+	this.breed = breed;
+	this.weight = weight;
+}
+
+function Duck (type, canFly) {
+	this.type = type;
+	this.canFly = canFly;
+}
+
+var fido = new Dog ("Fido", "Mixed", 38); //мы ставим ключевое слово new
+var fluffy = new Dog("Fluffy", "Poodle", 30);
+var spot = new Dog ("Spot", "Chihuahua", 10 );
+
+var dogs = [fido, fluffy, spot];
+
+for (let i = 0; i < dogs.length; i++) {
+	var size = "small";
+	if(dogs[i].weight > 10) {
+		size = "large";
+	}
+
+	console.log ("Dog: " + dogs[i].name
+				 + " is a " + size
+				 + " " + dogs[i].breed);
+}
